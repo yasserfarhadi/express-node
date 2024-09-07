6061bcbc0bbf749b6b7fa58d8950bde843172255
yasser farhadi <yasser.farhadee@gmail.com>
2024-09-07 18:27:38 +0430

    postgress

---
+++ pgconn.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const PoolClass = require('pg').Pool;
+
+const pool = new PoolClass({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'weatherTiler_development',
+  port: 5432,
+  password: '',
+});
+
+app.get('/', (req, res) => {
+  const query = 'SELECT * FROM city_weathers WHERE id > $1';
+  const scaryDataFromInternet = 36;
+
+  pool.query(query, [scaryDataFromInternet], (err, dbResponse) => {
+    console.log(dbResponse);
+    res.json(dbResponse);
+  });
+
+  pool.end();
+});
+
+app.listen(3000, () => console.log('Listening on 3000...'));
